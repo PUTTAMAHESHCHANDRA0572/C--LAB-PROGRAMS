b7d6e174abcb08ebaa23759ee0a3e80d198866b2
PUTTAMAHESHCHANDRA0572 <69686158+PUTTAMAHESHCHANDRA0572@users.noreply.github.com>
2020-12-21 21:37:06 +0530

    Add files via upload
    MATHOPERATIONS_DLL

---
+++ MATHOPERATIONS Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATHOPERATIONS_DLL
+{
+  public class Program
+    {
+        public int add(int a,int b,int c)
+        {
+            return a + b + c;
+        }
+        public int sub(int x,int y)
+        {
+            return x - y;
+        }
+        public int mul(int a,int b,int c)
+        {
+            return a * b * c;
+        }
+        public int div(int x,int y)
+        {
+            return x/y;
+        }
+        public int mod(int a,int b)
+        {
+            return a % b;
+        }
+        static void Main(string[] args)
+        {
+              
+        }
+    }
+}
