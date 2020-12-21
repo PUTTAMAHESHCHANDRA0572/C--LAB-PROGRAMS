fe8e5f7a139afe099c33d1f9c7a26409b7b43278
PUTTAMAHESHCHANDRA0572 <69686158+PUTTAMAHESHCHANDRA0572@users.noreply.github.com>
2020-12-21 19:13:28 +0530

    Add files via upload
    CONTACT SEARCH

---
+++ CONTACT SEARCH Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace CONTACT_SEARCH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text;
+            if(searchText==null||searchText=="")
+            {
+                MessageBox.Show("search string is empty");
+            }
+            else
+            {
+                //string contactsText = File.ReadAllText("");
+                StreamReader sReader = new StreamReader("C:\\Users\\Mahesh\\Desktop\\" +
+                    "C#LAB PROGRAMS\\contactsearch.txt");
+                string tempText = sReader.ReadLine();
+                while (tempText != "")
+                {
+
+
+                    if (searchText.Equals(tempText))
+                    {
+                        listBox1.Items.Add(searchText);
+                        listBox1.Items.Add(sReader.ReadLine());
+                        break;
+                    }
+                    tempText = sReader.ReadLine();
+                }
+            }
+        }
+    }
+}
