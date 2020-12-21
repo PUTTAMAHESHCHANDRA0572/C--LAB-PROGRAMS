af21af43fec385e7ab75628c6941dd7a308653d2
PUTTAMAHESHCHANDRA0572 <69686158+PUTTAMAHESHCHANDRA0572@users.noreply.github.com>
2020-12-21 21:36:04 +0530

    Add files via upload
    MATH_OPERATION

---
+++ MATH_OPERATION Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATH_OPERATIONS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            int res;
+            Console.WriteLine(Math.Min(a, b));
+            Console.WriteLine(Math.Max(a, b));
+            Console.WriteLine(Math.DivRem(b, a, out res));
+            Console.WriteLine(Math.BigMul(a, b));
+           
+            
+
+            Console.ReadLine();
+        }
+    }
+}
